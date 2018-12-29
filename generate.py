5c279e16ad3f857538799846e12611b7cf02eecf
赵晨 <zhaoc3@knownsec.com>
2018-12-29 19:01:43 +0800

    first commit

---
+++ generate.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python env
+# coding:utf8
+import os
+import re
+
+def generate(directory):
+    html_files = os.popen("find %s -iname '*.html'"%(directory))
+    jspattern = re.compile("/js/src/.+\.js", re.I)
+    csspattern = re.compile("/.+?\.css")
+    imgpattern = re.compile("(/images/.+?\.(jpg|png|jpeg|svg|gif|tif))")
+    for html in html_files:
+        html = html.rstrip('\n')
+        with open(html) as f:
+
+            context = f.read()
+            js = list(set(jspattern.findall(context)))
+            print js
+            css = list(set(csspattern.findall(context)))
+            print css
+            img = list(set([i[0] for i in imgpattern.findall(context)]))
+            print img
+    result = js + css + img
+    return result
+
+def generate_main_config(files):
+    string = ['location /index.html {']
+    for line in files:
+        string.append('http2_push %s'%(line))
+    string.append('}')
+
+    with open('nginx_http2_push.config', 'w') as f:
+        for line in string:
+            f.write(line+'\n')
+
+
+
+
+
+
+
+
+
+result = generate('/Users/zhaochen/Desktop/studylua/')
+generate_main_config(result)
